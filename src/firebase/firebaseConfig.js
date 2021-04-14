59c969ae312cccc235ebc090281f274662460978
matthew8541 <ytseng35@gatech.edu>
2021-04-14 20:34:30 +0800

    Upload Presentation Video

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,10 +1,10 @@
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyAWTyjxbD5lQuC5PWbDeuXScDDYuNXiCVk",
-  authDomain: "pumpedup-97f58.firebaseapp.com",
-  projectId: "pumpedup-97f58",
-  storageBucket: "pumpedup-97f58.appspot.com",
-  messagingSenderId: "393519285650",
-  appId: "1:393519285650:web:c5577ed31a1689be136d4b",
-  measurementId: "G-CFVYBXR9W0"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
