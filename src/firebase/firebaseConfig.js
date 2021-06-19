5b1fb10c59081eb3a1db05c92e0b56689963462b
matthew8541 <ytseng35@gatech.edu>
2021-06-19 17:24:06 +0800

    api keys fixed

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,10 +1,4 @@
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyAWTyjxbD5lQuC5PWbDeuXScDDYuNXiCVk",
-  authDomain: "pumpedup-97f58.firebaseapp.com",
-  projectId: "pumpedup-97f58",
-  storageBucket: "pumpedup-97f58.appspot.com",
-  messagingSenderId: "393519285650",
-  appId: "1:393519285650:web:c5577ed31a1689be136d4b",
-  measurementId: "G-CFVYBXR9W0"
+  // put your API keys here
 };
